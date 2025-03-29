a61fc35fafc96c7dcf5686693129ae31661640be
amireinidavid <realheritagecompany@gmail.com>
2025-03-29 10:41:44 +0100

    server update

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,7 +12,7 @@ export const prisma = new PrismaClient();
 const app = express();
 
 const corsOptions = {
-  origin: ["http://localhost:3000", "https://localhost:3001"], // Add your client URLs here
+  origin: process.env.CLIENT_URL, // Add your client URLs here
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
